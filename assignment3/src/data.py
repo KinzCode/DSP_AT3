4e34051b90a51b2742310dc07dd12a3ec5d08e3b
navid.m <navid.m108@yahoo.com>
2021-10-17 12:15:08 +1100

    changes on data.py

--- assignment3/src/data.py
+++ assignment3/src/data.py
@@ -4,103 +4,101 @@ from dataclasses import dataclass
 import pandas as pd
 import numpy as np
 from pathlib import Path
-
 file_path =
-rows = st.slider('Select the number of rows to be displayed', 0, 50, 5)
-
-
+rows = st.slider('Select the number of rows to be displayed',0,50,5 )
 @dataclass
 class Dataset:
-    name: str
-    df: pd.DataFrame
-
-    def get_name(self):
-        """
+  name: str
+  df: pd.DataFrame
+  
+  def get_name(self):
+    """
     Return filename of loaded dataset
     """
 
-        self.name = Path(file_path).stem
-        return None
+    self.name = Path(file_path).stem
+    return None
 
-    def get_n_rows(self):
-        """
+  def get_n_rows(self):
+    """
       Return number of rows of loaded dataset
     """
-        self.rows = df.shape[0]
-        return None
+    self.rows = df.shape[0]
+    return None
 
-    def get_n_cols(self):
-        """
+  def get_n_cols(self):
+    """
       Return number of columns of loaded dataset
     """
-        self.cols = df.shape[1]
-        return None
+    self.cols = df.shape[1]
+    return None
 
-    def get_cols_list(self):
-        """
+  def get_cols_list(self):
+    """
       Return list column names of loaded dataset
     """
-        self.cols_list = df.columns.tolist()
+    self.cols_list = df.columns.tolist()
 
-        return None
+    return None
 
-    def get_cols_dtype(self):
-        """
+  def get_cols_dtype(self):
+    """
       Return dictionary with column name as keys and data type as values
     """
-        self.dataTypeDict = dict(df.dtypes)
+    self.dataTypeDict = dict(df.dtypes)
 
-        return None
+    return None
 
-    def get_n_duplicates(self):
-        """
+  def get_n_duplicates(self):
+    """
       Return number of duplicated rows of loaded dataset
     """
-        return None
+    return None
 
-    def get_n_missing(self):
-        """
+  def get_n_missing(self):
+    """
       Return number of rows with missing values of loaded dataset
     """
-        self.missing = sum(df.apply(lambda x: sum(x.isnull().values), axis=1) > 0)
-        return None
+    self.missing = sum(df.apply(lambda x: sum(x.isnull().values), axis=1) > 0)
+    return None
 
-    def get_head(self, n=5):
-        """
+  def get_head(self, n=5):
+    """
       Return Pandas Dataframe with top rows of loaded dataset
     """
 
-        self.head = df.head(rows)
-        return None
+    self.head = df.head(rows)
+    return None
 
-    def get_tail(self, n=5):
-        """
+  def get_tail(self, n=5):
+    """
       Return Pandas Dataframe with bottom rows of loaded dataset
     """
-        self.tail = df.tail(rows)
-        return None
+    self.tail = df.tail(rows)
+    return None
 
-    def get_sample(self, n=5):
-        """
+  def get_sample(self, n=5):
+    """
       Return Pandas Dataframe with random sampled rows of loaded dataset
     """
-        self.sample = df.sample(rows)
-        return None
+    self.sample = df.sample(rows)
+    return None
 
-    def get_numeric_columns(self):
-        """
+  def get_numeric_columns(self):
+    """
       Return list column names of numeric type from loaded dataset
     """
-        return None
+    return None
 
-    def get_text_columns(self):
-        """
+  def get_text_columns(self):
+    """
       Return list column names of text type from loaded dataset
     """
-        return None
+    return None
 
-    def get_date_columns(self):
-        """
+  def get_date_columns(self):
+    """
       Return list column names of datetime type from loaded dataset
     """
-        return None
+    return None
+
