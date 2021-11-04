6fe57897c850a43bbed5501b70a637d2a4f56972
shangqingyang <471854953@qq.com>
2021-11-04 10:16:14 +0800

    made changes to text

--- assignment3/src/text.py
+++ assignment3/src/text.py
@@ -13,82 +13,94 @@ class TextColumn:
         """
         Return name of selected column
         """
-        self.name = self.col_name
-        return self.name
+        self.name = str(self.col_name)
+        return None
 
     def get_unique(self):
         """
         Return number of unique values for selected column
         """
-        self.unique = len(pd.unique(self.serie))
-        return self.unique
+        self.unique = str(len(pd.unique(self.serie)))
+        return None
 
     def get_missing(self):
         """
         Return number of missing values for selected column
         """
-        self.missing = self.serie.isnull().sum()
-        return self.missing
+        self.missing = str(self.serie.isnull().sum())
+        return None
 
     def get_empty(self):
         """
         Return number of rows with empty string for selected column
         """
-        self.empty = self.serie.isna().sum()
-        return self.empty
+        self.empty = str(self.serie.isna().sum())
+        return None
 
     def get_whitespace(self):
         """
         Return number of rows with only whitespaces for selected column
         """
-        self.white = len(self.serie.loc[self.serie.isnull()])
-        return self.white
+        self.white = str(len(self.serie.loc[self.serie.isnull()]))
+        return None
 
     def get_lowercase(self):
         """
         Return number of rows with only lower case characters for selected column
         """
-        self.lower = len(self.serie[self.serie.apply(lambda x: True if x.islower() else False)])
-        return self.lower
+        self.lower = str(len(self.serie[self.serie.apply(lambda x: True if str(x).islower() else False)]))
+        return None
 
     def get_uppercase(self):
         """
         Return number of rows with only upper case characters for selected column
         """
-        self.upper = len(self.serie[self.serie.apply(lambda x: True if x.isupper() else False)])
-        return self.upper
+        self.upper = str(len(self.serie[self.serie.apply(lambda x: True if str(x).isupper() else False)]))
+        return None
 
     def get_alphabet(self):
         """
         Return number of rows with only alphabet characters for selected column
         """
-        self.alpha = len(self.serie[self.serie.apply(lambda x: True if x.isalpha() else False)])
-        return self.alpha
+        self.alpha = str(len(self.serie[self.serie.apply(lambda x: True if str(x).isalpha() else False)]))
+        return None
 
     def get_digit(self):
         """
         Return number of rows with only numbers as characters for selected column
         """
-        self.digit = len(self.serie[self.serie.apply(lambda x: True if x.isdigit() else False)])
-        return self.digit
+        self.digit = str(len(self.serie[self.serie.apply(lambda x: True if str(x).isdigit() else False)]))
+        return None
 
     def get_mode(self):
         """
         Return the mode value for selected column
         """
         self.mode = self.serie.mode()
-        return self.mode
+        if len(self.mode) > 1:
+            self.mode = str(self.serie.mode()[0])
+        else:
+            self.mode = str(self.mode[0])
+        return None
 
     def get_barchart(self):
         """
         Return the generated bar chart for selected column
         """
-        self.barchart = st.bar_chart(self.serie)
+        occurences = pd.DataFrame(self.serie.value_counts())
+        self.barchart = st.bar_chart(occurences)
         return None
 
     def get_frequent(self):
         """
         Return the Pandas dataframe containing the occurrences and percentage of the top 20 most frequent values
         """
-        self.frequent = pd.DataFrame(self.serie.value_counts())
-        return self.frequent
+        occurences = pd.DataFrame(self.serie.value_counts()).reset_index()
+        percentage = pd.DataFrame(self.serie.value_counts(normalize=True)).reset_index()
+
+        self.frequency = occurences.merge(percentage, on='index', how='left')
+        self.frequency.rename(columns={self.frequency.columns[0]: 'value',
+                                       self.frequency.columns[1]: 'occurance',
+                                       self.frequency.columns[2]: 'precentage'},
+                              inplace=True)
+        return None
